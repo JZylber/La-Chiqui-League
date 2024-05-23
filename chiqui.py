0f7f5cbe49ab0995906ead14fe43b01b68f7be57
AgustinColinas <127337225+AgustinColinas@users.noreply.github.com>
2024-05-23 11:07:26 -0300

    casi-6

--- chiqui.py
+++ chiqui.py
@@ -2,65 +2,56 @@ import random
 import math
 from scipy.stats import poisson, binom
 
-class Equipo:
-    def __init__(self, nombre, ataque, defensa):
-        self.nombre = nombre
-        self.puntos = 0
-        self.goles_favor = 0
-        self.goles_contra = 0
-        self.ataque = ataque
-        self.defensa = defensa
-
-def simular_liga(equipos):
-    partidos = [(equipo1, equipo2) for equipo1 in equipos for equipo2 in equipos if equipo1 != equipo2]
-    
-    for equipo1, equipo2 in partidos:
-        goles_equipo1, goles_equipo2 = simular_partido(equipo1, equipo2)
-        
-        equipo1.goles_favor += goles_equipo1
-        equipo1.goles_contra += goles_equipo2
-        equipo2.goles_favor += goles_equipo2
-        equipo2.goles_contra += goles_equipo1
-        
-        if goles_equipo1 > goles_equipo2:
-            equipo1.puntos += 3
-        elif goles_equipo2 > goles_equipo1:
-            equipo2.puntos += 3
-        else:
-            equipo1.puntos += 1
-            equipo2.puntos += 1
-
-    tabla = [(equipo.nombre, equipo.puntos, equipo.goles_favor, equipo.goles_contra) for equipo in equipos]
-    tabla_ordenada = ordenar_tabla(tabla)
-    return tabla_ordenada
-
-if __name__ == "__main__":
-    equipos = [
-        Equipo("River", 0.9, 0.8),
-        Equipo("Boca", 0.6, 0.7),
-        Equipo("Independiente", 0.7, 0.6),
-        Equipo("San Lorenzo", 0.9, 0.9),
-        Equipo("Racing", 0.4, 0.3),
-        Equipo("Manchester City", 0.7, 0.8),
-        Equipo("Manchester united", 0.7, 0.6),
-        Equipo("Liverpool", 0.5, 0.6),
-        Equipo("Arsenal", 0.4, 0.5),
-        Equipo("Chelsea", 0.7, 0.5),
-        Equipo("Real Madrid", 0.9, 0.7),
-        Equipo("Barcelona", 0.8, 0.7),
-        Equipo("Atletico Madrid", 0.5, 0.9),
-        Equipo("Bayrn Munich", 0.8, 0.7),
-        Equipo("Borussia Dortmund", 0.6, 0.7),
-        Equipo("Inter", 0.7, 0.7),
-        Equipo("Milan", 0.9, 0.7),
-        Equipo("Juventus", 0.7, 0.7),
-        Equipo("Ajax", 0.7, 0.8),
-        Equipo("Porto", 0.8, 0.8),
-    ]
-
-    tabla_posiciones = simular_liga(equipos)
-    imprimir_tabla_posiciones=(tabla_posiciones)
-
+equipos = (
+    ("River", 0.9, 0.8),
+    ("Boca", 0.6, 0.7),
+    ("Independiente", 0.7, 0.6),
+    ("San Lorenzo", 0.9, 0.9),
+    ("Racing", 0.4, 0.3),
+    ("Manchester City", 0.7, 0.8),
+    ("Manchester united", 0.7, 0.6),
+    ("Liverpool", 0.5, 0.6),
+    ("Arsenal", 0.4, 0.5),
+    ("Chelsea", 0.7, 0.5),
+    ("Real Madrid", 0.9, 0.7),
+    ("Barcelona", 0.8, 0.7),
+    ("Atletico Madrid", 0.5, 0.9),
+    ("Bayrn Munich", 0.8, 0.7),
+    ("Borussia Dortmund", 0.6, 0.7),
+    ("Inter", 0.7, 0.7),
+    ("Milan", 0.9, 0.7),
+    ("Juventus", 0.7, 0.7),
+    ("Ajax", 0.7, 0.8),
+    ("Porto", 0.8, 0.8),
+)
+Tabla_pos = [{'equipo': equipo, 'ptos' : 0, 'gf': 0, 'gc': 0} for equipo, gf, gc in equipos]
+ 
+def simular_liga(equipos):  
+    n = 1
+    i = 0
+    while i < len(equipos):
+        while n < len(equipos):
+            golesA, golesB = simular_partido(equipos[i], equipos[n])
+            if golesA > golesB:
+                Tabla_pos[i]['ptos'] += 3
+            
+            if golesA < golesB:
+                Tabla_pos[n]['ptos'] +=3
+            else:
+                Tabla_pos[i]['ptos'] +=1
+                Tabla_pos[n]['ptos'] +=1
+            
+            Tabla_pos[i]['gf'] += golesA
+            Tabla_pos[i]['gc'] += golesB
+            
+            Tabla_pos[n]['gf'] += golesA
+            Tabla_pos[n]['gc'] += golesB
+            n +=1
+        i += 1
+        n = 0
+    Tabla_pos = ordenar_tabla(Tabla_pos) 
+    for equipo in Tabla_pos:
+        print(f"Equipo: {equipo['equipo']}, Puntos:{equipo['ptos']} GF: {equipo['gf']}, GC: {equipo['gc']}")
 
 def validar_equipo(equipo):
     assert (type(equipo) == tuple or type(equipo) == list) and len(equipo)== 3, f"El equipo debe ser una lista o tupla de 3 elementos, no {equipo}"
@@ -95,11 +86,4 @@ def orden_en_tabla(equipo):
 def ordenar_tabla(tabla):
     return sorted(tabla, key=orden_en_tabla, reverse=True)
 
-def imprimir_tabla_posiciones(tabla):
-    print("Tabla de Posiciones:")
-    print("--------------------")
-    print("{:<20} {:<8} {:<8} {:<8}".format("Equipo", "Puntos", "GF", "GC"))
-    print("--------------------")
-    for equipo in tabla:
-        print("{:<20} {:<8} {:<8} {:<8}".format(equipo[0], equipo[1], equipo[2], equipo[3]))
-    print("--------------------")
+simular_liga(equipos)
